5740ca0b59f228d4330e3677cc607273b9181a2c
Pedro Pereira <pbp333@gmail.com>
2018-01-11 15:48:54 +0000

    added configuration class

--- src/main/java/com/pbp333/springbootrest/springBootRest/Controller/WelcomeController.java
+++ src/main/java/com/pbp333/springbootrest/springBootRest/Controller/WelcomeController.java
@@ -1,11 +1,20 @@
 package com.pbp333.springbootrest.springBootRest.Controller;
 
+import com.pbp333.springbootrest.springBootRest.configuration.BasicConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class WelcomeController {
+
+    @Autowired
+    private BasicConfiguration configuration;
+
 
     @Value("${welcome.message}")
     private String welcomeMessage;
@@ -13,5 +22,16 @@ public class WelcomeController {
     @RequestMapping("/welcome")
     public String welcome(){
         return welcomeMessage;
+    }
+
+    @RequestMapping("/dynamic-configuration")
+    public Map dynamicConfiguration(){
+
+        Map map = new HashMap();
+
+        map.put("message", configuration.getMessage());
+        map.put("number", configuration.getNumber());
+        map.put("value", configuration.isValue());
+        return map;
     }
 }
